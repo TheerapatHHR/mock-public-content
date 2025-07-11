c9efb5db2b020afc6054e6edf4d7830182e7cc05
Theerapat Honghirun <theerapat.h@arise.tech>
2025-07-11 12:24:17 +0700

    remove redirect url

--- src/auth.ts
+++ src/auth.ts
@@ -69,10 +69,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       console.log("Redirect URL:", url);
       console.log("Base URL:", baseUrl);
     // Allows relative callback URLs
-    if (url.startsWith("/")) return `${baseUrl}:4200${url}`
+    if (url.startsWith("/")) return `${baseUrl}${url}`
  
     // Allows callback URLs on the same origin
-    if (new URL(url).origin === baseUrl) return `${url}:4200`
+    if (new URL(url).origin === baseUrl) return url
  
     return baseUrl
   }
